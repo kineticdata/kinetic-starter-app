ce882c2d3b76e8f6d6bf90afd845ebc10a739b7b
marcinzarycki <marcin.zarycki@kineticdata.com>
2023-11-07 11:14:42 -0600

    Updated bundle to use latest RKL version

--- craco.config.js
+++ craco.config.js
@@ -11,8 +11,28 @@ module.exports = {
         },
       });
 
+      // Fix for CJS files not being treated as JavaScript files
+      // https://github.com/facebook/create-react-app/issues/11889#issuecomment-1114928008
+      const fileLoaderRule = getFileLoaderRule(webpackConfig.module.rules);
+      if (!fileLoaderRule) {
+        throw new Error("File loader not found");
+      }
+      fileLoaderRule.exclude.push(/\.cjs$/);
 
       return webpackConfig;
     },
   },
 };
+
+function getFileLoaderRule(rules) {
+  for (const rule of rules) {
+    if ("oneOf" in rule) {
+      const found = getFileLoaderRule(rule.oneOf);
+      if (found) {
+        return found;
+      }
+    } else if (rule.test === undefined && rule.type === 'asset/resource') {
+      return rule;
+    }
+  }
+}
